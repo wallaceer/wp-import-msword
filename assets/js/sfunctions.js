2977b897c82e0c8df8d912dd08a30c6837fede32
wallaceer <santi.walter@gmail.com>
2022-10-26 16:31:51 +0200

    added access token validation

--- assets/js/sfunctions.js
+++ assets/js/sfunctions.js
@@ -2,19 +2,22 @@ jQuery(document).ready(function($) {
     var $ = jQuery.noConflict();
     $("#button_create").click(function(e) {
         e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "../wp-content/plugins/wp-import-word/cron/ws_job.php",
-            data: {
-                access_token: $("#access_token").val()
-            },
-            success: function(result) {
-                $("#result_create_posts").html(result);
-            },
-            error: function(result) {
-                $("#result_create_posts").html(result);
-            }
-        });
+        var accessToken = $("#access_token").val();
+        if(accessToken != ''){
+            $.ajax({
+                type: "POST",
+                url: "../wp-content/plugins/wp-import-word/cron/ws_job.php",
+                data: {
+                    access_token: accessToken
+                },
+                success: function(result) {
+                    $("#result_create_posts").html(result);
+                },
+                error: function(result) {
+                    $("#result_create_posts").html(result);
+                }
+            });
+        }
     });
 
 });
