504bcfa8ba3d8c48f698af9a02f4caf179a94873
wallaceer <ws@waltersanti.info>
2023-05-05 10:22:18 +0200

    changed branch for update

--- wp-import-word.php
+++ wp-import-word.php
@@ -43,7 +43,7 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
 );
 
 //Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('production');
+$myUpdateChecker->setBranch('main');
 
 //Optional: If you're using a private repository, specify the access token like this:
 $myUpdateChecker->setAuthentication('your-token-here');
