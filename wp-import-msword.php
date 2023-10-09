ba543fa1b42dab360d79ef0b10c23cdf673dd850
wallaceer <ws@waltersanti.info>
2023-10-09 15:16:32 +0200

    update name of file and version of plugin

---
+++ wp-import-msword.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Plugin Name: Wordpress Import MS Word
+ * Plugin URI: https://github.com/wallaceer/wp-import-msword/
+ * Description: Import content from Word document
+ * Author: WiTech
+ * Author URI: https://waltersanti.info
+ * Version: 2.3.1
+ * Requires at least: 5.2
+ * Requires PHP: 7.2
+ * Text Domain: wpimportword
+ * Domain Path: /languages
+ * License: GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+include __DIR__ . "/includes/ws_load.php";
+
+/**
+ * Load the plugin textdomain for localisation
+ * @since 2.0.0
+ */
+function wp_import_load_plugin_textdomain() {
+    load_plugin_textdomain( 'wpimportword', FALSE, plugin_basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'init', 'wp_import_load_plugin_textdomain' );
